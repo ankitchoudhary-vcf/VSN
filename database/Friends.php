07153e96eb82ec1340654145d788962c88633f01
ankit <ac2000choudhary@gmail.com>
2021-06-11 22:01:22 +0530

    Friends Database updated

--- database/Friends.php
+++ database/Friends.php
@@ -65,13 +65,37 @@ class Friends
 
     function getOtherConnection($request_from_id)
     {
-        $query = "SELECT * FROM chat_user_table  WHERE user_id !=$request_from_id AND NOT EXISTS (SELECT *  FROM friend_request WHERE user_id = friend_request.request_to_id)";
+        $query = "SELECT * FROM chat_user_table  WHERE user_id !=$request_from_id AND NOT EXISTS (SELECT *  FROM friend_request WHERE user_id = friend_request.request_to_id AND friend_request.request_from_id =$request_from_id)";
 
         $statement = $this->connect->prepare($query);
 
 		$statement->execute();
 
 		return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    function getFriendRequest($request_to_id)
+    {
+        $query = "SELECT * FROM chat_user_table JOIN friend_request ON friend_request.request_from_id = user_id WHERE friend_request.request_to_id = $request_to_id AND friend_request.request_status != 'Accepted'";
+
+        $statement = $this->connect->prepare($query);
+
+		$statement->execute();
+
+		return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    function AcceptFriendRequest()
+    {
+        $query = "UPDATE friend_request SET request_status = '$this->status' WHERE request_from_id = $this->request_to_id AND request_to_id = $this->request_from_id;
+                INSERT INTO friend_request(request_from_id, request_to_id, request_status) VALUE (:request_from_id, :request_to_id, :request_status);";   
+        $statement = $this->connect->prepare($query);
+        $statement->BindParam(':request_from_id', $this->request_from_id);
+        $statement->BindParam(':request_to_id', $this->request_to_id);
+        $statement->BindParam(':request_status', $this->status);
+
+		$statement->execute();   
+
     }
 }
 
