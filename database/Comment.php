114a3d6eb54c842bfef03ab6126be6c094f5e934
ankit <ac2000choudhary@gmail.com>
2021-06-20 09:25:37 +0530

    comment database added

---
+++ database/Comment.php
@@ -0,0 +1,42 @@
+<?php
+
+class Comment
+{
+    private $id;
+    private $post_id;
+    private $user_id;
+    private $comment;
+    private $connect;
+
+    function __construct(){ 
+        require_once('/Database_connection.php');
+        $database_object = new Database_connection;
+        $this->connect = $database_object->connect();
+    }
+
+    function setUserId($user_id){ 
+        $this->user_id = $user_id;
+    }
+    function setPostId($post_id){
+        $this->post_id = $post_id;
+    }
+    function setComment($comment){
+        $this->comment = $comment;
+    }
+
+    function getAllCommentsBypost_id(){
+        $query = "SELECT * FROM comment WHERE post_id = $this->post_id";
+        $statement = $this->connect->prepare($query);
+        $statement->execute();
+        $data  = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $data;
+    }
+    function saveComment(){
+        $query = "INSERT INTO comment (post_id, comment, user_id) VALUES($this->post_id, $this->comment, $this->user_id)";
+        $statement = $this->connect->prepare($query);
+        $statement->execute();
+    }
+
+}
+
+?>
